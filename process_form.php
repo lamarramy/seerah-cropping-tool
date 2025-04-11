c7e91826832e4596be58646424c3e5621b4ecfe9
lamarramy <168173703+lamarramy@users.noreply.github.com>
2025-04-11 19:52:46 +0200

    Add files via upload

---
+++ process_form.php
@@ -0,0 +1,41 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $name = $_POST['name'];
+    $phoneNumber = $_POST['phoneNumber'];
+    $address = $_POST['address'];
+
+    // Email settings
+    $to = "lamarramy.sh@gmail.com";
+    $subject = "New Order from Seera Custom Photo Magnet";
+    $headers = "From: no-reply@seera.com";
+
+    // Create email body
+    $message = "Name: $name\nPhone: $phoneNumber\nAddress: $address\n\n";
+
+    // Process uploaded images and attach them to the email
+    $attachments = [];
+    foreach ($_FILES['photos']['tmp_name'] as $key => $tmp_name) {
+        $file = $_FILES['photos']['tmp_name'][$key];
+        $file_name = $_FILES['photos']['name'][$key];
+        $file_type = $_FILES['photos']['type'][$key];
+        $file_content = chunk_split(base64_encode(file_get_contents($file)));
+
+        $attachments[] = "--boundary\r\n".
+                         "Content-Type: $file_type; name=\"$file_name\"\r\n".
+                         "Content-Disposition: attachment; filename=\"$file_name\"\r\n".
+                         "Content-Transfer-Encoding: base64\r\n\r\n".
+                         "$file_content\r\n";
+    }
+
+    // Combine all parts and send the email
+    $boundary = "----boundary";
+    $headers .= "\r\nContent-Type: multipart/mixed; boundary=\"$boundary\"\r\n";
+    $message = "--$boundary\r\nContent-Type: text/plain; charset=UTF-8\r\n\r\n$message\r\n";
+    $message .= implode("\r\n", $attachments);
+    $message .= "--$boundary--";
+
+    mail($to, $subject, $message, $headers);
+    echo "Thank you for your order! We will get back to you shortly.";
+}
+?>
